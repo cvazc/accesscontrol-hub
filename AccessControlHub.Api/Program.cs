9efae98875d537a782a1be3478d7a5af975ca9e2
Sebastián Contreras <145626352+cvazc@users.noreply.github.com>
2025-12-09 16:42:03 +0000

    Add ugly in-memory CRUD for Users directly in Program.cs

--- AccessControlHub.Api/Program.cs
+++ AccessControlHub.Api/Program.cs
@@ -1,3 +1,5 @@
+using AccessControlHub.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -17,6 +19,49 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/ping", () => "pong");
+var users = new List<User>()
+{
+    new User { Id = 1, Name = "John Doe", Email = "john@example.com" },
+    new User { Id = 2, Name = "Jane Smith", Email = "jane@example.com" }
+};
+
+app.MapGet("/users", () =>
+{
+    return users;
+});
+
+
+app.MapGet("/users{id}", (int id) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    return user is not null ? Results.Ok(user) : Results.NotFound();
+});
+
+app.MapPost("/users", (User newUser) =>
+{
+    newUser.Id = users.Max(u => u.Id) + 1;
+    users.Add(newUser);
+    return Results.Created($"/users/{newUser.Id}", newUser);
+});
+
+app.MapPut("/users{id}", (int id, User  updatedUser) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    if(user is null) return Results.NotFound();
+
+    user.Name = updatedUser.Name;
+    user.Email = updatedUser.Email;
+
+    return Results.Ok(user);
+});
+
+app.MapDelete("/users{id}", (int id) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    if(user is null) return Results.NotFound();
+
+    users.Remove(user);
+    return Results.Ok();
+});
 
 app.Run();
